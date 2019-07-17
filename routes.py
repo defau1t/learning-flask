84dfc680a192da73399d343878b106b5f1f6bf5c
defau1t <robin5693@gmail.com>
2019-07-17 08:54:25 -0600

    Added map to homepage.

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from models import db, User
-from forms import SignupForm, LoginForm
+from models import db, User, Place
+from forms import SignupForm, LoginForm, AddressForm
 import os
 
 app = Flask(__name__)
@@ -12,11 +12,11 @@ app.secret_key = "development-key"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
@@ -48,7 +48,7 @@ def login():
 
     if request.method == "POST":
         if form.validate() == False:
-            return render_tempalte("login.html", form=form)
+            return render_tempalte('login.html', form=form)
         else:
             email = form.email.data
             password = form.password.data
@@ -68,12 +68,34 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('index'))
 
-@app.route("/home")
+@app.route("/home", methods=['GET', 'POST'])
 def home():
     if 'email' not in session:
         return redirect(url_for('login'))
 
-    return render_template("home.html")
+    form = AddressForm()
+
+    places = []
+    my_coordinates = (37.4221, -122.0844)
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+        else:
+            # get the address
+            address = form.address.data
+
+            # query for places around it
+            p = Place()
+            my_coordinates = p.address_to_latlng(address)
+            places = p.query(address)
+
+            # return those results
+            return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
+    elif request.method == 'GET':
+        return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
